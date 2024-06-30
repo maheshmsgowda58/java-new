fcd6e1c236e52b463355a0d4b47369f5e2293ee9
Admin <Admin@10.10.2.66>
2024-06-30 22:16:06 +0530

    hiiiiii

--- helloworld/src/helloworld/helloworld.java
+++ helloworld/src/helloworld/helloworld.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class helloworld {
 	public static void main(String args[]) {
-		System.out.println("hello world hkhgjhgjhggvvgfvf");
+		System.out.println("hello world hkhgjhgjhggvvgfv 66866   f");
 	}
 
 }
