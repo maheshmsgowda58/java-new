c5285a61af65f609c4fdeda69740fcb42902768a
Admin <Admin@10.10.2.66>
2024-06-30 22:09:20 +0530

    commit it

--- helloworld/src/helloworld/helloworld.java
+++ helloworld/src/helloworld/helloworld.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class helloworld {
 	public static void main(String args[]) {
-		System.out.println("hello world");
+		System.out.println("hello world hkhgjhgjhggvvgfvf");
 	}
 
 }
